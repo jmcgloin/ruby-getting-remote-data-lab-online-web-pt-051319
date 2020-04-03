95e4f1e68bd93af5f1874a13ea6beed040593e75
Jason McGloin <jason.mcgloin@gmail.com>
2020-04-02 23:57:19 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'open-uri'
+
+class GetRequester
+	def initialize(url)
+	 @url = url
+	end
+	def get_response_body
+		uri = URI.parse(@url)
+		uri.open.string
+	end
+	def parse_json
+		JSON.parse(get_response_body)
+	end
+
+end
+
+# require 'open-uri'
+# url = "https://learn-co-curriculum.github.io/json-site-example/"
+# uri = URI.parse(url)
+# uri.open.string
